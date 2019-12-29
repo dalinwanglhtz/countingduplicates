d416976b8785a35ec91522624a58848af489b63d
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-29 14:26:08 +1100

    final version

--- src/com/codewardev/CountingDuplicates.java
+++ src/com/codewardev/CountingDuplicates.java
@@ -2,22 +2,17 @@ package com.codewardev;
 
 //https://www.codewars.com/kata/counting-duplicates/train/java
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class CountingDuplicates {
 
 	public static int duplicateCount(String text) {
 		String[] textArray = text.toLowerCase().split("");
-		List<String> result = new ArrayList<String>();
-		result = Arrays.stream(textArray)
+		return Arrays.stream(textArray)
 		.filter(x-> Collections.frequency(Arrays.asList(textArray), x) > 1).distinct()
-		.collect(Collectors.toList());
-		
-		return result.size();
+		.collect(Collectors.toList()).size();
 	}
 
 }
